c527134a4857ca7631391597200f8c12da8868bc
Paras Sharma <153740785+Paras-2005@users.noreply.github.com>
2026-01-17 19:21:49 +0530

    Create Rotate_array.c

---
+++ CODES/Rotate_array.c
@@ -0,0 +1,16 @@
+void reverse(int* nums, int l, int r) {
+    while (l < r) {
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
+        l++;
+        r--;
+    }
+}
+void rotate(int* nums, int numsSize, int k) {
+    if (numsSize == 0) return;
+    k = k % numsSize; 
+    reverse(nums, 0, numsSize - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, numsSize - 1);
+}
