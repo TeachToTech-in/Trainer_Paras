ba243a125e4455fb49c08bd6d0253cb629b1b504
Paras Sharma <153740785+Paras-2005@users.noreply.github.com>
2026-01-17 19:34:50 +0530

    Create length_of_last_word.c

---
+++ CODES/length_of_last_word.c
@@ -0,0 +1,12 @@
+int lengthOfLastWord(char* s) {
+     int len = strlen(s);
+    int count = 0;
+    int i = len - 1;
+    while (i >= 0 && s[i] == ' ')
+        i--;
+    while (i >= 0 && s[i] != ' ') {
+        count++;
+        i--;
+    }
+    return count;
+}
